b54f1ce6da6b9ff310bbff22ecb7e4cb18d64d33
Adrian <adrianorihuela@upeu.edu.pe>
2020-12-22 11:03:33 -0500

    chef gil

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   port: process.env.PORT || 3000,
   db:
     process.env.MONGODB ||
-    'mongodb+srv://admin:root@cluster0.rd0um.mongodb.net/DB_MovilidadAcademica?retryWrites=true&w=majority',
+    'mongodb+srv://admin:admin20@cluster0.rd0um.mongodb.net/DB_MovilidadAcademica?retryWrites=true&w=majority',
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const app = require('./app')
 const morgan = require('morgan')
 
 const config = require('./config/config')
-
-app.set('port', process.env.PORT || 3000);
+//
 
 app.listen(config.port, () => {
   console.log('Escuchando puerto: ' + config.port)
