14e0f6bf5ceafc114aa8735c3e0148783db534d9
Adrian <adrianorihuela@upeu.edu.pe>
2020-12-22 10:38:20 -0500

    f

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 
 const config = require('./config/config')
 
+app.set('port', process.env.PORT || 3000);
+
 app.listen(config.port, () => {
   console.log('Escuchando puerto: ' + config.port)
 })
